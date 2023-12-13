c7031c39694a06c8e9b8f1c7f54c17a54d258710
Renan <renanmarcelinodass@gmail.com>
2023-12-13 13:21:05 -0300

    Correção de alguns metodos e testes.

--- TestesUnitarios.Desafio.Tests/Usings.cs
+++ TestesUnitarios.Desafio.Tests/Usings.cs
@@ -1,1 +1,23 @@
 global using Xunit;
+
+namespace TestesUnitarios.Desafio.Console.Services
+
+    // ValidacoesLista.cs
+public class ValidacoesLista
+{
+    // Métodos existentes...
+
+    public int RetornarMaiorNumeroLista(List<int> lista)
+    {
+        // Implementação da lógica para retornar o maior número da lista
+        // ...
+        return 0; // Substitua pelo valor correto
+    }
+
+    public int RetornarMenorNumeroLista(List<int> lista)
+    {
+        // Implementação da lógica para retornar o menor número da lista
+        // ...
+        return 0; // Substitua pelo valor correto
+    }
+}
